3cb1c281b5b2fcf00960bd30b67b608a4c7a0ab5
Avokado-an <56486448+Avokado-an@users.noreply.github.com>
2020-07-06 19:29:47 +0300

    init commit

---
+++ src/main/java/com/anton/day7/main/Main.java
@@ -0,0 +1,36 @@
+package com.anton.day7.main;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int choice = -1;
+        while (choice != 0) {
+            System.out.println("1 - rus\t2 - german\tany other - eng\t0 - escape");
+            try {
+                choice = in.nextInt();
+            } catch (InputMismatchException ex) {
+                System.out.println("Wrong input. Default settings applied");
+            }
+            String country = "";
+            String language = "";
+            if (choice == 1) {
+                country = "ru";
+                language = "RU";
+            } else if (choice == 2) {
+                country = "de";
+                language = "DE";
+            }
+            Locale current = new Locale(language, country);
+            ResourceBundle rb = ResourceBundle.getBundle("data.text", current);
+            String congratulation1 = rb.getString("congratulation1");
+            System.out.println(congratulation1);
+            String congratulation2 = rb.getString("congratulation2");
+            System.out.println(congratulation2);
+        }
+    }
+}
